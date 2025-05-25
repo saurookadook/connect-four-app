4c856014d448a33d153ff753ce1bc0baa01d1368
Andy Maskiell <maskiella@gmail.com>
2025-05-25 13:49:58 -0400

    feat: add another test for AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  #gameType: string = 'Connect Four';
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  get gameType(): string {
+    return this.#gameType;
   }
 }
