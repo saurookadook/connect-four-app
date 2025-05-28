410c5ff69a31b6243682bb594af40fae018d545c
Andy Maskiell <maskiella@gmail.com>
2025-05-28 10:11:54 -0400

    feat: add more db constants

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -1,3 +1,9 @@
+import { getConnectionToken, getModelToken } from '@nestjs/mongoose';
+import { GameSession } from '@/game-engine/schemas/game-session.schema';
+
 export const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 export const MONGO_PORT = process.env.MONGO_PORT || 27017;
 export const MONGO_CONNECTION = `mongodb://${MONGO_HOST}:${MONGO_PORT}/connect-four`;
+
+export const MONGO_CONNECTION_TOKEN = getConnectionToken('DatabaseConnection');
+export const GAME_SESSION_MODEL_TOKEN = getModelToken(GameSession.name);
