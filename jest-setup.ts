e2120a4cd820a7619659fefe2ed849d9354d1a89
Andy Maskiell <maskiella@gmail.com>
2025-05-31 17:56:28 -0400

    chore: fix env var name, add listeners to jest-setup for mongoose connections events

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,8 +1,20 @@
 import { Db, MongoClient } from 'mongodb';
+import * as mongoose from 'mongoose';
 
 import { GAME_SESSION_MODEL_TOKEN, MONGO_HOST, MONGO_PORT } from '@/constants';
 
-global.__MONGO_URI__ = `mongodb://${MONGO_HOST}:${MONGO_PORT}/test`;
+global.__MONGO_URI__ = `mongodb://${MONGO_HOST}:${MONGO_PORT}/test-connect-four`;
+
+mongoose.set('debug', true);
+mongoose.connection.on('connected', () => {
+  console.log('[Mongoose] ---- Connected to MongoDB');
+});
+mongoose.connection.on('error', (error) => {
+  console.error('[Mongoose] ---- Encountered ERROR: ', error);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('[Mongoose] ---- Disconnected from MongoDB');
+});
 
 // let connection: MongoClient;
 // let db: Db;
