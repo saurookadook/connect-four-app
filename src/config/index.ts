695af975b05055c0f2ab2d0373e7e159947261df
Andy Maskiell <maskiella@gmail.com>
2025-06-07 13:27:11 -0400

    feat: add buildConnectionURI helper

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+
 const safeSetInt = (val: unknown, fallback: number): number =>
   typeof val === 'string' ? parseInt(val, 10) : fallback;
+
+export function buildConnectionURI(configService: ConfigService) {
+  const host = configService.get('database.host');
+  const port = configService.get('database.port');
+  const dbName = configService.get('database.dbName');
+
+  // console.log(`Connection URI parts: `, { host, port, dbName });
+  return `mongodb://${host}:${port}/${dbName}`;
+}
 
 export default () => ({
   port: safeSetInt(process.env.PORT, 3000),
