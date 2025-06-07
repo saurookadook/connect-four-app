2b5250908c7d46cdc691d9ab0719905f3b8a6b14
Andy Maskiell <maskiella@gmail.com>
2025-06-07 15:53:34 -0400

    feat: create initial GameEngineController and routes

---
+++ src/game-engine/game-engine.controller.ts
@@ -0,0 +1,39 @@
+import { UUID } from 'crypto';
+import * as uuid from 'uuid';
+import { Body, Controller, Get, Query, Param, Post } from '@nestjs/common';
+
+import { CreateGameSessionDTO } from '@game-engine/dtos/game-session.dto';
+import { GameSessionService } from '@game-engine/session/game-session.service';
+import { GameEngineService } from '@game-engine/game-engine.service';
+
+@Controller('games')
+export class GameEngineController {
+  constructor(
+    private readonly gameEngineService: GameEngineService,
+    private readonly gameSessionService: GameSessionService,
+  ) {}
+
+  @Get(':playerId/all')
+  async getAllGamesForPlayer(
+    @Param('playerId') playerId: string,
+    @Query('activeOnly') activeOnly: boolean = false,
+  ) {
+    if (!uuid.validate(playerId)) {
+      throw new TypeError(
+        "[GameEngineController.getAllGamesForPlayer] Parameter for 'playerId' must be a valid UUID.",
+      );
+    }
+
+    return await this.gameSessionService.findAllForPlayer(playerId as UUID);
+  }
+
+  @Post('start')
+  async startGame(
+    @Body() createGameSessionDTO: CreateGameSessionDTO, // force formatting
+  ) {
+    return await this.gameEngineService.startGame({
+      playerOneID: createGameSessionDTO.playerOneID,
+      playerTwoID: createGameSessionDTO.playerTwoID,
+    });
+  }
+}
--- src/game-engine/game-engine.module.ts
+++ src/game-engine/game-engine.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { GameEngineController } from '@game-engine/game-engine.controller';
 import { GameSessionModule } from '@game-engine/session/game-session.module';
 import { GameEngineService } from '@game-engine/game-engine.service';
 
 @Module({
-  controllers: [],
+  controllers: [GameEngineController],
   imports: [GameSessionModule],
   providers: [GameEngineService],
 })
