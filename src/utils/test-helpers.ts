be8c70a68392094cfda45efa6467b818455b33a6
Andy Maskiell <maskiella@gmail.com>
2025-06-08 13:50:20 -0400

    feat: create mongo testing helper

--- src/utils/test-helpers.ts
+++
@@ -1,8 +0,0 @@
-export const createMockModel = <T>(model: T) => ({
-  new: jest.fn().mockResolvedValue(model),
-  constructor: jest.fn().mockResolvedValue(model),
-  find: jest.fn(),
-  create: jest.fn(),
-  save: jest.fn(),
-  exec: jest.fn(),
-});
