9ddbbadce77a162f67e3a7b4d9051152c0f3a715
Andy Maskiell <maskiella@gmail.com>
2025-06-08 14:32:57 -0400

    feat: port over tests for game-logic-engine classes

---
+++ src/game-logic-engine/__tests__/GameLogicEngine.test.ts
@@ -0,0 +1,263 @@
+import { UUID, randomUUID } from 'crypto';
+
+import { GameLogicEngine, LogicSession } from '@/game-logic-engine';
+import { GameSessionStatus, PlayerColor } from '@/game-logic-engine/constants';
+
+describe('GameLogicEngine', () => {
+  const mockPlayerOneID: UUID = randomUUID();
+  const mockPlayerTwoID: UUID = randomUUID();
+  let gameEngine: GameLogicEngine;
+  let logicSession: LogicSession;
+
+  beforeEach(() => {
+    gameEngine = new GameLogicEngine();
+  });
+
+  afterEach(() => {
+    if (logicSession != null) {
+      logicSession.board.reset();
+    }
+  });
+
+  describe('startGame', () => {
+    test('creates a new game logic session with player IDs', () => {
+      const newLogicSession = gameEngine.startGame({
+        playerOneID: mockPlayerOneID,
+        playerTwoID: mockPlayerTwoID,
+      });
+
+      expect(newLogicSession).toBeDefined();
+      expect(newLogicSession).toBeInstanceOf(LogicSession);
+      expect(newLogicSession.playerOneID).toBe(mockPlayerOneID);
+      expect(newLogicSession.playerTwoID).toBe(mockPlayerTwoID);
+    });
+  });
+
+  describe('handleMove', () => {
+    beforeEach(() => {
+      logicSession = gameEngine.startGame({
+        playerOneID: mockPlayerOneID,
+        playerTwoID: mockPlayerTwoID,
+      });
+    });
+
+    // TODO: fix method implementation
+    test.skip("updates board state and active player for 'sessionRef'", () => {
+      const {
+        board: { state: boardState },
+      } = logicSession;
+      const targetColIndex = 3;
+
+      expect(boardState[targetColIndex].at(-1)?.state).toBeNull();
+      expect(logicSession.activePlayer).toBe(PlayerColor.RED);
+
+      gameEngine.handleMove({
+        columnIndex: targetColIndex,
+        playerID: mockPlayerOneID,
+        sessionRef: logicSession,
+      });
+
+      expect(boardState[targetColIndex].at(-1)?.state).toBe(PlayerColor.RED);
+      expect(logicSession.activePlayer).toBe(PlayerColor.BLACK);
+
+      gameEngine.handleMove({
+        columnIndex: targetColIndex,
+        playerID: mockPlayerTwoID,
+        sessionRef: logicSession,
+      });
+
+      expect(boardState[targetColIndex].at(-1)?.state).toBe(PlayerColor.BLACK);
+      expect(logicSession.activePlayer).toBe(PlayerColor.RED);
+    });
+
+    // TODO: fix method implementation
+    test.skip("updates board state with winning move for 'sessionRef'", () => {
+      populateBoardWithOneMoveTilWin(logicSession);
+
+      const targetColIndex = 0;
+
+      gameEngine.handleMove({
+        columnIndex: targetColIndex,
+        playerID: mockPlayerOneID,
+        sessionRef: logicSession,
+      });
+
+      expect(logicSession.board.state[targetColIndex].at(-1)?.state).toBe(
+        PlayerColor.RED,
+      );
+      expect(logicSession.activePlayer).toBe(PlayerColor.RED);
+    });
+  });
+
+  describe('checkForWin', () => {
+    describe('no win conditions', () => {
+      let logicSession: LogicSession;
+
+      beforeEach(() => {
+        logicSession = gameEngine.startGame({
+          playerOneID: mockPlayerOneID,
+          playerTwoID: mockPlayerTwoID,
+        });
+      });
+
+      test('simple case', () => {
+        logicSession.board.updateBoardState({
+          columnIndex: 3,
+          playerColor: PlayerColor.RED,
+        });
+        logicSession.board.updateBoardState({
+          columnIndex: 2,
+          playerColor: PlayerColor.BLACK,
+        });
+        logicSession.board.updateBoardState({
+          columnIndex: 3,
+          playerColor: PlayerColor.RED,
+        });
+        logicSession.board.updateBoardState({
+          columnIndex: 3,
+          playerColor: PlayerColor.BLACK,
+        });
+        logicSession.board.updateBoardState({
+          columnIndex: 4,
+          playerColor: PlayerColor.RED,
+        });
+
+        expect(
+          gameEngine.checkForWin(logicSession.board, logicSession.activePlayer),
+        ).toBe(false);
+      });
+    });
+
+    describe('win conditions', () => {
+      let logicSession: LogicSession;
+
+      beforeEach(() => {
+        logicSession = gameEngine.startGame({
+          playerOneID: mockPlayerOneID,
+          playerTwoID: mockPlayerTwoID,
+        });
+      });
+
+      test('simple case', () => {
+        populateBoardWithSimpleWin(logicSession);
+
+        expect(
+          gameEngine.checkForWin(logicSession.board, logicSession.activePlayer),
+        ).toBe(true);
+      });
+    });
+  });
+
+  describe('endGame', () => {
+    test('correctly handles a game session with a winning condition', () => {
+      const logicSession: LogicSession = gameEngine.startGame({
+        playerOneID: mockPlayerOneID,
+        playerTwoID: mockPlayerTwoID,
+      });
+      populateBoardWithSimpleWin(logicSession);
+
+      gameEngine.endGame(logicSession);
+
+      expect(logicSession.status).toBe(GameSessionStatus.COMPLETED);
+      expect(logicSession.activePlayer).toBe(PlayerColor.RED);
+    });
+  });
+});
+
+// ---------------------------------------------------------------------------
+// Helper functions
+// ---------------------------------------------------------------------------
+function populateBoardWithOneMoveTilWin(logicSessionRef: LogicSession): void {
+  // RED at (3, 0)
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 3,
+    playerColor: PlayerColor.RED,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 2,
+    playerColor: PlayerColor.BLACK,
+  });
+  // RED at (2, 1)
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 2,
+    playerColor: PlayerColor.RED,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 1,
+    playerColor: PlayerColor.BLACK,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 1,
+    playerColor: PlayerColor.RED,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 0,
+    playerColor: PlayerColor.BLACK,
+  });
+  // RED at (1, 2)
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 1,
+    playerColor: PlayerColor.RED,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 0,
+    playerColor: PlayerColor.BLACK,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 0,
+    playerColor: PlayerColor.RED,
+  });
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 3,
+    playerColor: PlayerColor.BLACK,
+  });
+}
+
+/**
+ * @example
+ * ```txt
+ *   0   1   2   3   4   5   6
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │ 0
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │ 1
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │ R │   │   │   │   │   │   │ 2
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │ R │ R │   │   │   │   │   │ 3
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │ B │ R │ R │ B │   │   │   │ 4
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │ B │ B │ B │ R │   │   │   │ 5
+ * └───┴───┴───┴───┴───┴───┴───┘
+ * ```
+ */
+function populateBoardWithSimpleWin(logicSessionRef: LogicSession): void {
+  populateBoardWithOneMoveTilWin(logicSessionRef);
+
+  // RED at (0, 3)
+  logicSessionRef.board.updateBoardState({
+    columnIndex: 0,
+    playerColor: PlayerColor.RED,
+  });
+}
+
+/**
+ * TEMPLATE
+ * @example
+ * ```txt
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │
+ * └───┴───┴───┴───┴───┴───┴───┘
+ * ```
+ */
---
+++ src/game-logic-engine/__tests__/LogicSession.test.ts
@@ -0,0 +1,118 @@
+import { randomUUID } from 'crypto';
+import { inspect } from 'util';
+
+import { LogicSession } from '@/game-logic-engine';
+import {
+  BOARD_COLS,
+  BOARD_ROWS,
+  PlayerColor,
+} from '@/game-logic-engine/constants';
+
+describe('LogicSession', () => {
+  const mockPlayerOneID = randomUUID();
+  const mockPlayerTwoID = randomUUID();
+
+  describe('constructor', () => {
+    test('should initialize with empty board', () => {
+      const session = new LogicSession({
+        playerOneID: mockPlayerOneID,
+        playerTwoID: mockPlayerTwoID,
+      });
+      const {
+        board: { state: boardState },
+        playerOneID,
+        playerTwoID,
+      } = session;
+
+      expect(playerOneID).toBe(mockPlayerOneID);
+      expect(playerTwoID).toBe(mockPlayerTwoID);
+
+      expect(boardState).toHaveLength(BOARD_COLS);
+      expect(boardState[0]).toHaveLength(BOARD_ROWS);
+
+      for (let i = 0; i < BOARD_COLS; i++) {
+        for (let j = 0; j < BOARD_ROWS; j++) {
+          expect(boardState[i][j]).toEqual({
+            state: null,
+            column: i,
+            row: j,
+          });
+        }
+      }
+    });
+  });
+
+  describe('updateBoard', () => {
+    let session: LogicSession;
+
+    beforeEach(() => {
+      session = new LogicSession({
+        playerOneID: mockPlayerOneID,
+        playerTwoID: mockPlayerTwoID,
+      });
+    });
+
+    test('should update board with player color', () => {
+      const columnOne = 0;
+      const columnTwo = 1;
+      const columnThree = 2;
+
+      const {
+        board: { state: boardState },
+      } = session;
+
+      session.updateBoard({ column: columnOne, playerID: mockPlayerOneID });
+      expect(boardState[columnOne].at(-1)!.state).toBe(PlayerColor.RED);
+      session.updateBoard({ column: columnOne, playerID: mockPlayerTwoID });
+      expect(boardState[columnOne].at(-2)!.state).toBe(PlayerColor.BLACK);
+
+      session.updateBoard({ column: columnTwo, playerID: mockPlayerOneID });
+      expect(boardState[columnTwo].at(-1)!.state).toBe(PlayerColor.RED);
+      session.updateBoard({ column: columnThree, playerID: mockPlayerTwoID });
+      expect(boardState[columnThree].at(-1)!.state).toBe(PlayerColor.BLACK);
+
+      session.updateBoard({ column: columnTwo, playerID: mockPlayerOneID });
+      expect(boardState[columnTwo].at(-2)!.state).toBe(PlayerColor.RED);
+      session.updateBoard({ column: columnTwo, playerID: mockPlayerTwoID });
+      expect(boardState[columnTwo].at(-3)!.state).toBe(PlayerColor.BLACK);
+    });
+
+    test('should throw error for invalid column', () => {
+      const negativeColumnValue = -1;
+      expect(() => {
+        session.updateBoard({
+          column: negativeColumnValue,
+          playerID: mockPlayerOneID,
+        });
+      }).toThrow(
+        `Invalid 'columnIndex' value ('${negativeColumnValue}'): Must be between 0 and ${BOARD_COLS - 1}`,
+      );
+
+      expect(() => {
+        session.updateBoard({ column: BOARD_COLS, playerID: mockPlayerOneID });
+      }).toThrow(
+        `Invalid 'columnIndex' value ('${BOARD_COLS}'): Must be between 0 and ${BOARD_COLS - 1}`,
+      );
+    });
+
+    test('should throw error if column is full', () => {
+      for (let i = 0; i < BOARD_ROWS; i++) {
+        session.updateBoard({
+          column: 0,
+          playerID: i % 2 === 0 ? mockPlayerOneID : mockPlayerTwoID,
+        });
+      }
+
+      expect(() => {
+        session.updateBoard({ column: 0, playerID: mockPlayerOneID });
+      }).toThrow('Column 0 is full');
+    });
+
+    test('should throw error for unknown player ID', () => {
+      const unknownPlayerID = randomUUID();
+      expect(() => {
+        session.updateBoard({ column: 0, playerID: unknownPlayerID });
+      }).toThrow(`Unknown playerID: '${unknownPlayerID}'`);
+    });
+  });
+});
