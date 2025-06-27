56d2d1b628d332033f77ed689fbe89bb70c071c9
Andy Maskiell <maskiella@gmail.com>
2025-06-27 16:54:51 -0400

    feat: create Public decorator, LocalAuthGuard, and LocalStrategy classes, add Public decorator to getHello route

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+import { AuthenticationService } from '../authentication.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authenticationService: AuthenticationService) {
+    super({
+      usernameField: 'username',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authenticationService.validatePlayer({
+      username,
+      unhashedPassword: password,
+    });
+
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    return user;
+  }
+}
