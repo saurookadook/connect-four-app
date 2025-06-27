1bec189637a2896eee8ef8e9d8a97f71b8867013
Andy Maskiell <maskiella@gmail.com>
2025-06-27 18:19:09 -0400

    feat: update .env examples, add initial configuration for express-session in main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as session from 'express-session';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.SESSION_SECRET == null) {
+    throw new Error("'SESSION_SECRET' environment variable is not set.");
+  }
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: true },
+    }),
+  );
 
   // Additional configuration available here: https://github.com/expressjs/cors#configuration-options
   app.enableCors();
