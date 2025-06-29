857c340594e373f69550813e50bcf650a9c285c3
Andy Maskiell <maskiella@gmail.com>
2025-06-29 19:09:22 -0400

    feat: create and configure test_script :]

--- src/scripts/scripts.module.ts
+++ src/scripts/scripts.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@/database/database.module';
+import { GameSessionModule } from '@/game-engine/session/game-session.module';
+import { PlayerModule } from '@/player/player.module';
+import { TestCommand } from './commands/test.command';
 
 @Module({
-  imports: [DatabaseModule],
-  providers: [],
+  imports: [
+    DatabaseModule, // force formatting
+    GameSessionModule,
+    PlayerModule,
+  ],
+  providers: [
+    TestCommand, // force formatting
+  ],
 })
 export class ScriptsModule {}
