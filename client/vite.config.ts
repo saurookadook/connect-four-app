47f3ca8cd52fbf1e8d18bd32e12234be4645a3e5
Andy Maskiell <maskiella@gmail.com>
2025-07-10 17:25:21 -0400

    chore: copy over various config files for client from ui repo

---
+++ client/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="vitest/config" />
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __dirname = path.resolve();
+
+const { SERVER_PORT } = process.env;
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@ConnectFour': path.resolve(__dirname, './src/pages/ConnectFour/'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${SERVER_PORT}`,
+      },
+      '/connect-ws': {
+        target: 'ws://localhost:8090',
+        ws: true,
+        rewrite: (path) => path.replace(/^\/connect-ws/, ''),
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    // globals: true,
+    include: [
+      '**/*.{test,spec}.{js,jsx,ts,tsx}', // force formatting
+      '**/__tests__/**/*.{ts,tsx}',
+    ],
+    sequence: {
+      hooks: 'list',
+    },
+    setupFiles: ['./vitest.setup.ts'],
+  },
+});
