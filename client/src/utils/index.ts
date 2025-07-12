80f40bc814cf637f8a5a763137c10eb570a1b03c
Andy Maskiell <maskiella@gmail.com>
2025-07-12 11:42:27 -0400

    feat: refactor client-side websocket utils

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,48 +1,66 @@
-let ws: WebSocket;
+// TODO: move to `common` workspace
+type Nullable<T> = T | null;
 
-// see proxy settings in vite.config.ts
-const WS_CONNECTION_URL = 'ws://localhost:8090/connect-ws';
+class WebSocketManager {
+  #WS_CONNECTION_URL: string | URL;
+  #ws: Nullable<WebSocket>;
 
-try {
-  ws = new WebSocket(WS_CONNECTION_URL);
+  constructor(connectionURL?: string | URL) {
+    this.#WS_CONNECTION_URL = connectionURL ?? 'ws://localhost:8090/connect-ws';
+    this.#ws = null;
+  }
 
-  ws.addEventListener('open', (event) => {
-    console.log(
-      '    [WebSocket] Opening WebSocket connection    '.padStart(60, '-').padEnd(120, '-'),
-    );
-    console.log('\n');
-    console.log({ event });
-    console.log('\n');
-  });
+  initializeConnection() {
+    try {
+      const wsConn = this.getOpenWSConn();
 
-  ws.addEventListener('message', (event) => {
-    console.log('    [WebSocket] Receiving message!     '.padStart(60, '-').padEnd(120, '-'));
-    console.log('\n');
-    console.log({ event });
-    console.log('\n');
-  });
-} catch (error: unknown) {
-  console.error('ERROR creating WebSocket instance', error);
+      wsConn.addEventListener('open', (event) => {
+        console.log(
+          '    [WebSocket] Opening WebSocket connection    '.padStart(60, '-').padEnd(120, '-'),
+        );
+        console.log('\n');
+        console.log({ event });
+        console.log('\n');
+      });
+
+      wsConn.addEventListener('message', (event) => {
+        console.log('    [WebSocket] Receiving message!     '.padStart(60, '-').padEnd(120, '-'));
+        console.log('\n');
+        console.log({ event });
+        console.log('\n');
+      });
+    } catch (error: unknown) {
+      console.error('ERROR creating WebSocket instance', error);
+    }
+  }
+
+  getOpenWSConn(): WebSocket {
+    if (this.#ws == null) {
+      this.#ws = new WebSocket(this.#WS_CONNECTION_URL);
+    }
+    return this.#ws;
+  }
+
+  closeWSConn() {
+    if (
+      this.#ws != null &&
+      (this.#ws.readyState === this.#ws.OPEN || this.#ws.readyState === this.#ws.CONNECTING)
+    ) {
+      console.log(
+        '    [WebSocket] Closing WebSocket connection    '.padStart(60, '-').padEnd(120, '-'),
+      );
+      this.#ws.close();
+      this.#ws = null;
+    }
+  }
+
+  get ws(): Nullable<WebSocket> {
+    return this.#ws;
+  }
 }
 
-const openWSConn = () => {
-  if (ws == null) {
-    ws = new WebSocket(WS_CONNECTION_URL);
-  }
-  return ws;
-};
+const wsManager = new WebSocketManager();
 
-const closeWSConn = () => {
-  if (ws != null && (ws.readyState === ws.OPEN || ws.readyState === ws.CONNECTING)) {
-    console.log(
-      '    [WebSocket] Closing WebSocket connection    '.padStart(60, '-').padEnd(120, '-'),
-    );
-    ws.close();
-    // @ts-expect-error: This is on purpose :]
-    ws = null;
-  }
-};
-
-export { ws, openWSConn, closeWSConn };
+export { wsManager };
 export * from './deeplyMerge';
 export * from './typeGuards';
