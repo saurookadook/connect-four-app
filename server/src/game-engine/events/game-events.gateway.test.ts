3d340766de1e5d97cca808d7a0a2bc565362edb3
Andy Maskiell <maskiella@gmail.com>
2025-07-13 12:17:40 -0400

    feat: refactor GameEventsGateway to keep internal map of open clients by game sessions, sorta fix GameEventsGateway tests

--- server/src/game-engine/events/game-events.gateway.test.ts
+++ server/src/game-engine/events/game-events.gateway.test.ts
@@ -1,15 +1,25 @@
-import { randomUUID } from 'node:crypto';
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
-import { reduce } from 'rxjs/operators';
+import ws from 'ws';
+import WSMock from 'jest-websocket-mock';
 
+import { mockFirstPlayer, mockSecondPlayer } from '@/__mocks__/playerMocks';
 import { databaseProviders } from '@/database/database.providers';
 import { GameEngineModule } from '../game-engine.module';
-import { GameEventsGateway } from './game-events.gateway';
+import { GameEventsGateway, type GameSessionMap } from './game-events.gateway';
+import { createNewGameSessionDocumentMock } from '@/__mocks__/gameSessionsMocks';
 
 describe('GameEventsGateway', () => {
-  const mockPlayerOneID = randomUUID();
-  const mockPlayerTwoID = randomUUID();
+  const mockPlayerOneID = mockFirstPlayer.playerID;
+  const mockPlayerTwoID = mockSecondPlayer.playerID;
+  const mockGameSession = createNewGameSessionDocumentMock({
+    playerOneID: mockPlayerOneID,
+    playerTwoID: mockPlayerTwoID,
+  });
+  const mockGameSessionID = mockGameSession._id.toString();
+
   let gateway: GameEventsGateway;
+  let activeGameSession: GameSessionMap;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,6 +31,28 @@ describe('GameEventsGateway', () => {
     }).compile();
 
     gateway = module.get(GameEventsGateway);
+    // @ts-expect-error: Not sure why these types don't line up better?
+    gateway.server = new WSMock(`ws://localhost:8090`).server;
+
+    const activeGamesMap = gateway.activeGamesMap;
+    activeGamesMap.set(mockGameSessionID, new Map());
+    activeGameSession = activeGamesMap.get(mockGameSessionID) as GameSessionMap;
+    const mockWebSocketClient = jest.fn(() => {
+      return {
+        send: jest.fn((...args) => args),
+      };
+    });
+    activeGameSession?.set(
+      mockPlayerOneID,
+      // @ts-expect-error: Until I can figure out a better way to mock the client
+      mockWebSocketClient(),
+    );
+
+    activeGameSession?.set(
+      mockPlayerTwoID,
+      // @ts-expect-error: Until I can figure out a better way to mock the client
+      mockWebSocketClient(),
+    );
   });
 
   describe('move', () => {
@@ -28,41 +60,41 @@ describe('GameEventsGateway', () => {
       const firstTimestamp = new Date();
       const secondTimestamp = new Date(firstTimestamp.getTime() + 2000);
 
-      const firstCall = await gateway.onEvent({
+      const firstMakeMoveEvent = {
         columnIndex: 1,
+        gameSessionID: mockGameSession._id.toString(),
         playerID: mockPlayerOneID,
         timestamp: firstTimestamp,
-      });
+      };
 
-      firstCall
-        .pipe(reduce((acc, item) => [...acc, item], []))
-        .subscribe((results) => {
-          expect(results).toHaveLength(1);
-          results.forEach((result, index) => {
-            expect(result.event).toBe('resolved-move');
-            expect(result.data.columnIndex).toBe(1);
-            expect(result.data.playerID).toBe(mockPlayerOneID);
-            expect(result.data.timestamp).toBe(firstTimestamp);
-          });
-        });
+      await gateway.onMakeMoveEvent(firstMakeMoveEvent);
 
-      const secondCall = await gateway.onEvent({
-        columnIndex: 2,
+      expect(
+        // @ts-expect-error: Until I can figure out a better way to mock the client
+        activeGameSession?.get(mockPlayerOneID).send,
+      ).toHaveBeenNthCalledWith(1, JSON.stringify(firstMakeMoveEvent));
+      expect(
+        // @ts-expect-error: Until I can figure out a better way to mock the client
+        activeGameSession?.get(mockPlayerTwoID).send,
+      ).toHaveBeenNthCalledWith(1, JSON.stringify(firstMakeMoveEvent));
+
+      const secondMakeMoveEvent = {
+        columnIndex: 1,
+        gameSessionID: mockGameSession._id.toString(),
         playerID: mockPlayerTwoID,
         timestamp: secondTimestamp,
-      });
+      };
 
-      secondCall
-        .pipe(reduce((acc, item) => [...acc, item], []))
-        .subscribe((results) => {
-          expect(results).toHaveLength(1);
-          results.forEach((result, index) => {
-            expect(result.event).toBe('resolved-move');
-            expect(result.data.columnIndex).toBe(2);
-            expect(result.data.playerID).toBe(mockPlayerTwoID);
-            expect(result.data.timestamp).toBe(secondTimestamp);
-          });
-        });
+      await gateway.onMakeMoveEvent(secondMakeMoveEvent);
+
+      expect(
+        // @ts-expect-error: Until I can figure out a better way to mock the client
+        activeGameSession?.get(mockPlayerOneID).send,
+      ).toHaveBeenNthCalledWith(2, JSON.stringify(secondMakeMoveEvent));
+      expect(
+        // @ts-expect-error: Until I can figure out a better way to mock the client
+        activeGameSession?.get(mockPlayerTwoID).send,
+      ).toHaveBeenNthCalledWith(2, JSON.stringify(secondMakeMoveEvent));
     });
   });
 });
