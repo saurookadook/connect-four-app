b517a5134aed92619e8d43b14b18fb0e363c9c41
Andy Maskiell <maskiella@gmail.com>
2025-07-13 18:59:18 -0400

    feat: add createMockGameSession function

--- client/src/__mocks__/gameSessionsMocks.ts
+++ client/src/__mocks__/gameSessionsMocks.ts
@@ -19,6 +19,33 @@ const playerCombinations = [
   [mockPlayers[2].playerID, mockPlayers[1].playerID],
 ];
 
+export function createMockGameSession({ id, ...args }: { id?: UUID } & Partial<GameSessionMock>) {
+  const { playerOneID, playerTwoID } = (function () {
+    if (args.playerOneID == null && args.playerTwoID == null) {
+      const [firstPlayerCombination] = playerCombinations[0];
+      return {
+        playerOneID: firstPlayerCombination[0],
+        playerTwoID: firstPlayerCombination[1],
+      };
+    } else {
+      return {
+        playerOneID: args.playerOneID,
+        playerTwoID: args.playerTwoID,
+      };
+    }
+  })();
+
+  return {
+    // TODO: this should be a MongoDB `ObjectId`
+    id: id ?? randomUUID(),
+    moves: [],
+    playerOneID,
+    playerTwoID,
+    status: GameSessionStatus.ACTIVE,
+    ...args,
+  };
+}
+
 export const unstartedGameSessionsMock: GameSessionMock[] = playerCombinations.map(
   ([playerOneID, playerTwoID]) => {
     return {
