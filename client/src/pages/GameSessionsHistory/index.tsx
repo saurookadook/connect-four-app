f7a86b0397380551fe8d2a7c6db6811e6d296eb2
Andy Maskiell <maskiella@gmail.com>
2025-07-15 14:31:40 -0400

    chore: clean up GameSessionsHistory component

--- client/src/pages/GameSessionsHistory/index.tsx
+++ client/src/pages/GameSessionsHistory/index.tsx
@@ -1,12 +1,8 @@
 import { AllGameSessions, PlayerGameSessions } from './components';
 import { FlexRow } from '@/layouts';
-import { useAppStore } from '@/store';
 import './styles.css';
 
 export function GameSessionsHistory() {
-  const { appState, appDispatch } = useAppStore();
-  const { gameSessions, player } = appState;
-
   return (
     <section id="game-sessions-history">
       <h2>{`🗒️ Game Sessions History 🗒️`}</h2>
