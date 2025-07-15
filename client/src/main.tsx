e6d29f5265936e42642f6e5cd9b0f52c50c3935f
Andy Maskiell <maskiella@gmail.com>
2025-07-15 15:52:54 -0400

    chore: more clean up in main entry for client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react';
+import { StrictMode, type PropsWithChildren } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app/App';
 import { AppStateProvider } from './store';
 import './index.css';
 
-function InitApp() {
+function InitApp({ children }: PropsWithChildren) {
   const enableStrictMode = window.localStorage.getItem('cfStrictMode');
 
   return enableStrictMode != null && enableStrictMode ? (
-    <StrictMode>
-      <AppStateProvider>
-        <App />
-      </AppStateProvider>
-    </StrictMode>
+    <StrictMode>{children}</StrictMode>
   ) : (
-    <AppStateProvider>
-      <App />
-    </AppStateProvider>
+    children
   );
 }
 const rootEl = document.getElementById('root') as HTMLElement;
 
-createRoot(rootEl).render(<InitApp />);
+createRoot(rootEl).render(
+  <InitApp>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </InitApp>,
+);
