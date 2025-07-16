25c25a96300c9bb20fdc5a11b19fd6a3f7c9f362
Andy Maskiell <maskiella@gmail.com>
2025-07-16 10:20:31 -0400

    fix: add DatabaseModule to GameEngineModule imports

--- server/src/game-engine/game-engine.module.ts
+++ server/src/game-engine/game-engine.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { DatabaseModule } from '@/database/database.module';
 import { GameEngineController } from './game-engine.controller';
 import { GameSessionsModule } from './sessions/game-sessions.module';
 import { GameEngineService } from './game-engine.service';
 
 @Module({
   controllers: [GameEngineController],
-  imports: [GameSessionsModule],
+  imports: [DatabaseModule, GameSessionsModule],
   providers: [GameEngineService],
   exports: [GameEngineService],
 })
