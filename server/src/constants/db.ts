bea54de43970fa4bc23f67cfe2fcd809e18a3057
Andy Maskiell <maskiella@gmail.com>
2025-07-17 11:26:21 -0400

    feat: add BOARD_STATE_MODEL_TOKEN to constants

--- server/src/constants/db.ts
+++ server/src/constants/db.ts
@@ -1,7 +1,10 @@
 import { getConnectionToken, getModelToken } from '@nestjs/mongoose';
+
+import { BoardState } from '@/game-engine/schemas/board-states.schema';
 import { GameSession } from '@/game-engine/schemas/game-session.schema';
 import { Player } from '@/player/schemas/player.schema';
 
 export const MONGO_CONNECTION_TOKEN = getConnectionToken('DatabaseConnection');
+export const BOARD_STATE_MODEL_TOKEN = getModelToken(BoardState.name);
 export const GAME_SESSION_MODEL_TOKEN = getModelToken(GameSession.name);
 export const PLAYER_MODEL_TOKEN = getModelToken(Player.name);
