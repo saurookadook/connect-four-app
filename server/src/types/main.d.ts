fefb428e33283cd1f895665a455cb11ce0336855
Andy Maskiell <maskiella@gmail.com>
2025-07-18 10:28:18 -0400

    feat: move player-related types and update imports

--- server/src/types/main.d.ts
+++ server/src/types/main.d.ts
@@ -1,14 +1,23 @@
 import { UUID } from 'node:crypto';
 import { Types } from 'mongoose';
 
+export type BoardCell = {
+  cellState: PlayerDetails['playerID'] | null;
+  col: number;
+  row: number;
+};
+
 export type PlayerDetails = {
   playerID: UUID;
   playerObjectID: Types.ObjectId;
   username: string;
 };
 
-export type BoardCell = {
-  cellState: PlayerDetails['playerID'] | null;
-  col: number;
-  row: number;
+export type PlayerMove = {
+  columnIndex: number;
+  /** @note String representation of Mongo `ObjectId` */
+  gameSessionID: string;
+  playerID: UUID;
+  /** @todo Need to change this to `number` */
+  timestamp: Date;
 };
