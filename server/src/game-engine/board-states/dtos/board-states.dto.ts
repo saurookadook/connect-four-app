c7ccfa821899f2c4cd261548616af8b03a76a46c
Andy Maskiell <maskiella@gmail.com>
2025-07-18 10:35:31 -0400

    feat: add dtos for BoardState, move BoardState schema into board-states directory

---
+++ server/src/game-engine/board-states/dtos/board-states.dto.ts
@@ -0,0 +1,35 @@
+import { UUID } from 'node:crypto';
+import { Exclude, Expose } from 'class-transformer';
+
+import { BaseDTO, PartialBaseDTO } from '@/dtos/base.dto';
+import { BoardCell, PlayerMove } from '@/types/main';
+
+@Exclude()
+export class BoardStateDTO extends BaseDTO {
+  @Expose()
+  gameSessionID: string;
+
+  @Expose()
+  state: BoardCell[][];
+}
+
+@Exclude()
+export class CreateBoardStateDTO {
+  @Expose()
+  gameSessionID: BoardStateDTO['gameSessionID'];
+
+  @Expose()
+  moves?: PlayerMove[];
+}
+
+@Exclude()
+export class UpdateGameSessionDTO extends PartialBaseDTO {
+  @Expose()
+  gameSessionID: BoardStateDTO['gameSessionID'];
+
+  @Expose()
+  move?: PlayerMove;
+
+  @Expose()
+  state?: BoardStateDTO['state'];
+}
