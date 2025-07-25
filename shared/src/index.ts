47b88d0e294f1b2462156ce3451db25b69f63e4b
Andy Maskiell <maskiella@gmail.com>
2025-07-25 15:56:25 -0400

    fix: remove gameLogicEngine alias in shared exports

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,5 @@
 export * from './constants';
-export * as gameLogicEngine from './game-logic-engine';
+export * from './game-logic-engine';
 export { log as sharedLog } from './logger';
 export type * from './types/main.d';
 export * from './utils';
