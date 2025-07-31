a684191790e06c3a019a4086bd381d4a8d2339ea
Andy Maskiell <maskiella@gmail.com>
2025-07-31 15:39:25 -0400

    feat: add initial implementation for Matchmaking page component

--- client/src/pages/Matchmaking/index.tsx
+++ client/src/pages/Matchmaking/index.tsx
@@ -1,9 +1,66 @@
+import { useEffect, type MouseEvent } from 'react';
+
+import { sharedLog, type PlayerID } from '@connect-four-app/shared';
+import { LoadingState } from '@/components';
+import { FlexColumn, FlexRow } from '@/layouts';
+import { fetchPlayersData, makeStartGameRequest } from '@/store/actions';
+import { useAppStore } from '@/store';
+import './styles.css';
+
+const logger = sharedLog.getLogger(Matchmaking.name);
+
 export function Matchmaking() {
-  // TODO: :]
+  const { appState, appDispatch } = useAppStore();
+  const { matchmaking, player } = appState;
+
+  useEffect(() => {
+    if (matchmaking.playersDataRequestInProgress || matchmaking.playersData != null) {
+      return;
+    }
+
+    fetchPlayersData({
+      dispatch: appDispatch,
+      currentPlayerID: player.playerID as PlayerID,
+    });
+  }, [
+    matchmaking.playersDataRequestInProgress,
+    matchmaking.playersData,
+    player.playerID,
+  ]);
+
+  function handleOnStartGameClick(
+    event: MouseEvent<HTMLButtonElement>,
+    playerTwoID: PlayerID,
+  ) {
+    logger.debug(
+      `[${Matchmaking.name}.${handleOnStartGameClick.name}] playerTwoID: '${playerTwoID}' \n`,
+    );
+    console.dir(event);
+  }
 
   return (
     <section id="matchmaking">
       <h2>{`⚔️ Matchmaking ⚔️`}</h2>
+
+      <FlexColumn>
+        {matchmaking.playersData == null ? (
+          <LoadingState />
+        ) : (
+          <ul>
+            {matchmaking.playersData.map(({ playerID, username }, index) => {
+              return (
+                <li key={playerID} className="flex-row">
+                  <span>{`${username} ('${playerID}')`}</span>
+
+                  <button onClick={(e) => handleOnStartGameClick(e, playerID)}>
+                    Challenge to Match
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </FlexColumn>
     </section>
   );
 }
