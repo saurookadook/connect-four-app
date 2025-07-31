9a1a9ed90d517d1be39af4ed9d613ad540e8c842
Andy Maskiell <maskiella@gmail.com>
2025-07-31 11:10:18 -0400

    feat: add types for matchmaking reducer

---
+++ client/src/store/matchmaking/reducer.types.ts
@@ -0,0 +1,24 @@
+import CombineReducers from '@saurookkadookk/react-utils-combine-reducers';
+
+import { type Nullable, type PlayerID } from '@connect-four-app/shared';
+
+type PlayerData = {
+  playerID: PlayerID;
+  username: string;
+};
+
+export type MatchmakingStateSlice = {
+  playersDataRequestInProgress: boolean;
+  playersData: PlayerData[];
+};
+
+export type PlayerAction = CombineReducers.ReducerAction<{
+  playersData?: MatchmakingStateSlice['playersData'];
+}>;
+
+export type CombinedMatchmakingStateSlice = {
+  playersData: CombineReducers.ArgsTuple<
+    MatchmakingStateSlice['playersData'], // force formatting
+    PlayerAction
+  >;
+};
