aa4839879129b1d85320fb214750e5d9b7b2e189
Andy Maskiell <maskiella@gmail.com>
2025-08-01 07:35:26 -0400

    feat: add test case for starting game from Matchmaking page

--- client/src/pages/Matchmaking/index.test.tsx
+++ client/src/pages/Matchmaking/index.test.tsx
@@ -1,20 +1,25 @@
+import { cleanup, screen, waitFor, within } from '@testing-library/react';
+import renderWithContext from '#saurookkadookk/react-utils-render-with-context';
 import {
   afterAll, // force formatting
+  afterEach,
   beforeEach,
   describe,
   expect,
   it,
   vi,
 } from 'vitest';
-import renderWithContext from '#saurookkadookk/react-utils-render-with-context';
-import { screen, waitFor, within } from '@testing-library/react';
 
 import { mockPlayers } from '@/__mocks__/playerMocks';
 import { PLAYER_DETAILS_LS_KEY } from '@/constants';
+import { createEmptyBoard } from '@/pages/GameSession/utils';
 import { AppStateProvider } from '@/store';
 import {
   createFetchMock,
+  expectGameBoardToBeVisibleAndCorrect,
   expectHeadingToBeVisible,
+  expectMatchmakingPageToBeVisibleAndCorrect,
+  getMatchmakingContainer,
   WithMemoryRouter,
 } from '@/utils/testing';
 
@@ -39,13 +44,17 @@ describe('Matchmaking', () => {
     vi.clearAllMocks();
   });
 
+  afterEach(() => {
+    cleanup();
+  });
+
   afterAll(() => {
     fetchMock.mockRestore();
     vi.clearAllMocks();
   });
 
   it('renders correctly', async () => {
-    const { container, getByRole } = renderWithContext(
+    const { container } = renderWithContext(
       <MatchmakingWithRouter />, // force formatting
       AppStateProvider,
       {
@@ -58,33 +67,54 @@ describe('Matchmaking', () => {
       },
     );
 
+    await expectMatchmakingPageToBeVisibleAndCorrect({
+      screenRef: screen,
+      containerRef: container,
+      testPlayersData: [mockFirstPlayer, mockThirdPlayer],
+    });
+  });
+
+  it("correctly starts a new game when clicking the challenge button next to a player's username", async () => {
+    const { container, user } = renderWithContext(
+      <MatchmakingWithRouter />, // force formatting
+      AppStateProvider,
+      {
+        state: {
+          player: {
+            playerID: mockSecondPlayer.playerID,
+            username: mockSecondPlayer.username,
+          },
+        },
+      },
+    );
+
+    await expectMatchmakingPageToBeVisibleAndCorrect({
+      screenRef: screen,
+      containerRef: container,
+      testPlayersData: [mockFirstPlayer, mockThirdPlayer],
+    });
+
+    const { playerID: playerTwoID, username: playerTwoUsername } = mockFirstPlayer;
+    const matchmakingContainerEl = getMatchmakingContainer(container);
+    const playerListItem = within(matchmakingContainerEl)
+      .getByText(`${playerTwoUsername} ('${playerTwoID}')`, {
+        exact: false,
+      })
+      .closest('li') as HTMLLIElement;
+
+    await user.click(within(playerListItem).getByRole('button'));
+
     await expectHeadingToBeVisible({
       screenRef: screen,
       level: 2,
-      name: /Matchmaking/,
+      name: /Connect Four/,
     });
 
-    await waitFor(() => {
-      expect(getByRole('list')).toBeVisible();
-    });
-
-    const matchmakingContainerEl = // force formatting
-      container.querySelector('#matchmaking') as HTMLSelectElement;
-
-    const playerListItemEls =
-      await within(matchmakingContainerEl).findAllByRole('listitem');
-
-    expect(playerListItemEls).toHaveLength(2);
-    [mockFirstPlayer, mockThirdPlayer].forEach((expectedPlayer, index) => {
-      const listItemEl = playerListItemEls[index];
-
-      expect(listItemEl).toBeVisible();
-
-      const { playerID, username } = expectedPlayer;
-      const playerTextEl = within(listItemEl).getByText(`${username} ('${playerID}')`, {
-        exact: false,
-      });
-      expect(playerTextEl).toBeVisible();
+    await expectGameBoardToBeVisibleAndCorrect({
+      containerRef: container,
+      boardCells: createEmptyBoard(),
+      playerOneID: mockSecondPlayer.playerID,
+      playerTwoID: playerTwoID,
     });
   });
 });
